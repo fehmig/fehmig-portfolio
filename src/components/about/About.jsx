294fa507aa7ad5236c2204a144ff92ad030bfb61
Fehmi Günay <gunay.fehmi14@gmail.com>
2023-12-11 22:06:36 +0300

    cv updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/about.jpg"
-import CV from "../../assets/FehmiGünayCV-TrEn.pdf"
+import CV from "../../assets/FehmiGünay-CV-TR-EN.pdf"
 import Info from "./Info"
 import { useTranslation } from '../translate/TranslateContext'
 
